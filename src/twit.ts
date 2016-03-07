743556fe8cc5086267fcb5ef842d42350df23573
Tsubasa Namiki <namikingsoft@gmail.com>
2016-03-07 18:52:27 +0900

    Change getting tweet method to Stream API @WIP

--- src/twit.ts
+++ src/twit.ts
@@ -2,10 +2,6 @@ import {Robot, Response} from 'hubot'
 import Config from './config/Twit'
 const Twitter = require("twitter") // @todo d.ts
 
-interface FetchData {
-  statuses: Array<Tweet>
-}
-
 interface Tweet {
   id_str: number
   text: string
@@ -13,19 +9,10 @@ interface Tweet {
     name: string
     screen_name: string
   }
+  lang: string
 }
 
-interface Search {
-  name: string
-  text: string
-  sinceId: number
-}
-
-interface BrainData {
-  searchList: Array<Search>
-}
-
-module.exports = (robot: Robot<BrainData>) => {
+module.exports = (robot: Robot<any>) => {
 
   const queue: Array<string> = []
 
@@ -38,70 +25,27 @@ module.exports = (robot: Robot<BrainData>) => {
 
   const fetch = () => {
     const client = new Twitter(Config.API_KEYS)
-    const searchList = robot.brain.data.searchList
-    searchList.forEach((search: Search, index: number) => {
-      const param = {
-        q: search.text,
-        count: Config.MAX_FETCH_COUNT,
-        since_id: search.sinceId,
-      }
-      client.get('search/tweets', param, (err: Error, data: FetchData) => {
-        const tweets = data.statuses
-        if (err) {
-          const message = JSON.stringify(err, null, '  ')
-          console.log(`Error getting tweets: ${message}`)
-          return
-        }
-        if (tweets.length === 0) {
-          return
-        }
-        search.sinceId = tweets[0].id_str
-        robot.brain.data.searchList[index] = search
-        tweets.reverse().forEach(tweet => {
+    const option = {
+      track: 'docker,react,redux,electron,typescript',
+    }
+    client.stream('statuses/filter', option, (stream: any) => {
+      stream.on('data', (tweet: Tweet) => {
+        if (tweet.lang === 'ja') {
           const user = tweet.user
           const text = `${tweet.text} - ${user.name}@${user.screen_name}`
           const url = `http://twitter.com/${user.screen_name}/status/${tweet.id_str}`
           const message = `${text}\n${url}`
           queue.push(message)
-        })
+        }
+      })
+      stream.on('error', (err: Error) => {
+        setTimeout(15000, fetch)
       })
     })
   }
 
   robot.brain.on('loaded', () => {
-    if (!robot.brain.data.searchList) {
-      robot.brain.data.searchList = []
-    }
-    setInterval(fetch, 1000 * 30)
+    fetch()
     setInterval(send, 1000 * 10)
-  })
-
-  robot.respond(/twit fetch/i, fetch)
-
-  robot.respond(/twit reset/i, res => {
-    robot.brain.data.searchList = []
-    res.reply("OK. Search list was cleared")
-  })
-
-  robot.respond(/twit add ([a-zA-Z0-9_]+) (.*)/i, res => {
-    const name = res.match[1]
-    const text = res.match[2]
-    robot.brain.data.searchList.push({
-      name,
-      text,
-      sinceId: 1,
-    })
-    res.reply(`Added: ${name} -> ${text}`)
-  })
-
-  robot.respond(/twit rm ([a-zA-Z0-9_]+)/i, res => {
-    name = res.match[1]
-    robot.brain.data.searchList =
-      robot.brain.data.searchList.filter((x: Search) => x.name != name)
-    res.reply(`Removed: ${name}`)
-  })
-
-  robot.respond(/twit list/i, res => {
-    res.reply("\n" + JSON.stringify(robot.brain.data.searchList, null, '  '))
   })
 }
